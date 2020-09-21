fe30edd7bb0c0adbb042fccd235e580ffb8ec829
Zakaria Soufiani <mz.soufiani@gmail.com>
2020-09-21 14:37:21 +1000

    trigger deploy

--- simple-api/handler.py
+++ simple-api/handler.py
@@ -4,21 +4,9 @@ import json
 def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "input": event,
     }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
